74fc2d99e295d426fc79381116f8b5306953cada
jiangweilee <spain7lee@gmail.com>
2014-12-02 17:29:25 +0800

    split function

--- program.js
+++ program.js
@@ -1,13 +1,14 @@
 var through =require('through');
+var split = require('split');
 
-var tr = through(write,end);
+var tr = through(write);
+var i=1;
+process.stdin.pipe(split()).pipe(tr).pipe(process.stdout);
+function write (line) {
+  if(i%2==1)
+    this.queue(line.toString().toLowerCase()+'\n');
+  else
+    this.queue(line.toString().toUpperCase()+'\n');
 
-process.stdin.pipe(tr).pipe(process.stdout);
-
-function write(buf){
-
-  this.queue(buf.toString().toUpperCase());
-
+    i++
 }
-
-function end () { this.queue(null) }
