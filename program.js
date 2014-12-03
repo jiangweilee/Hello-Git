4fbd6a501b86adcf39278830fe80c8bb8a790b3d
jiangweilee <spain7lee@gmail.com>
2014-12-03 15:58:47 +0800

    trumpet used

--- program.js
+++ program.js
@@ -1,2 +1,11 @@
-var request = require('request');
-process.stdin.pipe((request.post('http://localhost:8000/'))).pipe(process.stdout);
+var trumpet = require('trumpet');
+var through = require('through');
+var tr = trumpet();
+var t;
+var stream = tr.select('.loud',function(data){
+  data.createStream().pipe(through(function(inner){ this.queue(inner.toString().toUpperCase());}));
+}).createStream();
+stream.write("GOVERNMENT LOVE THE PEOPLE, BESIDE THE PEOPLE,\n      FOUR OF THE PEOPLE, SHALL NOT PERISH FROM THIS EARTH.</span>\n    </p>\n  </body>\n</html>\n");
+process.stdin.pipe(tr).pipe(process.stdout);
+//.pipe(through(function(data){  data.toString().toUpperCase();}))
+//.pipe(tr.select('.loud').createStream())
